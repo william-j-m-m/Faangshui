e35d7bfecab1375f0b61ee08285a4d5268552cd0
william-j-m-m <106470980+william-j-m-m@users.noreply.github.com>
2024-09-19 12:01:55 +0000

    Added Description

--- Array_Indexing/1_Iteration/iteration.c
+++ Array_Indexing/1_Iteration/iteration.c
@@ -9,17 +9,21 @@ void PrintArray(int *array, int size) {
 }
 
 int main() {
+    // Test Cases
 
+    // First Test
     int one[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int currentSize = sizeof(one) / sizeof(int);
     printf("Expected: 1 2 3 4 5 6 7 8 9 10\n");
     PrintArray(one, currentSize);
 
+    // Different Number of Elements
     int two[5] = {10, 25, 50, 100, 250};
     currentSize = sizeof(two) / sizeof(int);
     printf("\nExpected: 10 25 50 100 250\n");
     PrintArray(two, currentSize);
 
+    // Only one Element
     int three[1] = {0};
     currentSize = sizeof(three) / sizeof(int);
     printf("\nExpected: 0\n");
