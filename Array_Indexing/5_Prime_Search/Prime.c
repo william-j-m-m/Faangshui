8b9e856af05b5265c5fffec8fb71f759f9e1fcf6
william-j-m-m <106470980+william-j-m-m@users.noreply.github.com>
2024-09-20 17:36:33 +0000

    Finished the Challenge

--- Array_Indexing/5_Prime_Search/Prime.c
+++ Array_Indexing/5_Prime_Search/Prime.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-// Assuming there is always a prime number in the array
-
 /**
- * @brief 
+ * @brief Test whether a numver is prime, or not
  * 
- * @param testNumber 
- * @return int 
+ * @param testNumber The number to be tested if it is prime
+ * @return int If it is Prime, Returns 1, Otherwise 0
  */
 int IsPrime(int testNumber) {
     // 1 is not Prime
@@ -14,13 +12,13 @@ int IsPrime(int testNumber) {
         return 0;
     }
     // Checking for any even numbers that aren't 2
-    if (int testNumber % 2 == 0 && testNumber != 2) {
+    if (testNumber % 2 == 0 && testNumber != 2) {
         return 0;
     }
 
     // Checking for any other factors
     // Only checking up to the square root of number to prevent wasted iterations.
-    for (int n = 3; n * n < testNumber; n += 2) {
+    for (int n = 3; n * n <= testNumber; n += 2) {
         if (testNumber % n == 0) {
             return 0;
         }
@@ -29,8 +27,7 @@ int IsPrime(int testNumber) {
     // Return 1 (True)
     return 1;
     }
-    
-}
+
 
 /**
  * @brief Returns the first prime in an array
@@ -38,11 +35,49 @@ int IsPrime(int testNumber) {
  * @param arr Pointer to the first element in the array
  * @param elements Number of elements in the array
  * @return int The First Prime Number in the Array
+ * Returns -1 if no prime is found
  */
 int FirstPrime(int *arr, int elements) {
-
+    for (int n = 0; n < elements; n++) {
+        if (IsPrime(arr[n]) == 1) {
+            return arr[n];
+        }
+    }
+    printf("\nError: No Prime Found!\n");
+    return -1;
 }
 
 int main() {
+    // Test Cases
+    // Prime Numbers for Test Cases found on Wikipedia
 
+    // First Test
+    int one[5] = {10, 3455, 3317, 1, 2};
+    if (FirstPrime(one, 5) == 2) {
+        printf("Passed Test!\n\tExpected: 2\n\tReturned: %d\n", FirstPrime(one, 5));
+    }
+    else {
+        printf("Failed Test!\n\tExpected: 2\n\tReturned: %d\n", FirstPrime(one, 5));
+    }
+
+    // Second Test, Multiple Primes
+    int two[7] = {1, 9, 3557, 2, 3, 5, 7};
+    if (FirstPrime(two, 7) == 3557) {
+        printf("Passed Test!\n\tExpected: 3557\n\tReturned: %d\n", FirstPrime(two, 7));
+    }
+    else {
+        printf("Failed Test!\n\tExpected: 3557\n\tReturned: %d\n", FirstPrime(two, 7));
+    }
+
+    // No Primes
+    int three[5] = {4, 6, 9, 25, 27};
+    if (FirstPrime(three, 3) == -1) {
+        printf("Passed Test!\n\tExpected: -1\n\tReturned: %d\n", FirstPrime(three, 5));
+    }
+    else {
+        printf("Failed Test!\n\tExpected: -1\n\tReturned: %d\n", FirstPrime(three, 5));
+    }
+
+
+    return 0;
 }
